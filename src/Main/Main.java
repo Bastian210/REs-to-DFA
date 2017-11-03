10fdba2ca725c9b1692b8e57dfb54b045b8bd799
Your Name <you@example.com>
2017-11-03 18:29:30 +0800

    update

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,6 +5,8 @@ import Operate.SplitCode;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -13,6 +15,15 @@ import java.util.Scanner;
  * Created by 费慧通 on 2017/10/25.
  */
 public class Main {
+    public static void Write(String s){
+        try {
+            FileWriter fw = new FileWriter("src/resources/output.txt",true);
+            fw.write(s+"\n");
+            fw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args){
         Match match = new Match();
@@ -20,7 +31,7 @@ public class Main {
         String[] code = splitCode.getCodeFromFile();
         for(int i=0;i<code.length;i++){
             if(code[i].length()>0) {
-                System.out.println(match.getToken(code[i]));
+                Write(match.getToken(code[i]).toString());
             }
         }
     }
